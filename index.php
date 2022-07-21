104dd6142224ce30654eb62cf3bbdec16aaa6182
Allan McCafferty <allan247archivist@gmail.com>
2022-07-21 22:03:09 +0100

    Add Stylesheet

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <title>PHP Countries</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
         <style>
             a
             {
